c6dee390b78975542f473172e9d276cbb95b183d
weihongweic <31128202+weihongweic@users.noreply.github.com>
2019-04-28 13:31:06 +0800

    Update start.sh

--- demo-mesh-user/userService/start.sh
+++ demo-mesh-user/userService/start.sh
@@ -4,4 +4,4 @@ mkdir -p /opt/tsf/app_config/apis
 cp  $GOPATH/src/demo-mesh-user/userService/spec.yaml /opt/tsf/app_config/
 cp -r / $GOPATH/src/demo-mesh-user/userService/apis /opt/tsf/app_config/
 cd  $GOPATH/src/demo-mesh-user/userService/
-python ./userService.py 8089 1>./logs/user.log 2>&1 &
+python ./userService.py 8089 1>./logs/user.log 2>&1
